6f5133aacfd9e56680dbbb929466305ab5ba6f29
ormon <ormonkenenbaev@gmail.com>
2021-04-03 12:01:07 +0600

    models created

--- ask_project/poll/models.py
+++ ask_project/poll/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Poll(models.Model):
+    question = models.CharField(max_length=50, blank=False)
+    dateTime = models.DateTimeField(auto_now_add=True, blank=False)
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    answer = models.CharField(max_length=100, blank=False)
+    asking = models.ForeignKey('poll.Poll', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.answer
