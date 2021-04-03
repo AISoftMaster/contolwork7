5dd888143317b4fa73440d882b4b6544b2386b3d
ormon <ormonkenenbaev@gmail.com>
2021-04-03 14:11:08 +0600

    plus poll_update

--- ask_project/poll/urls.py
+++ ask_project/poll/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PollListView, PollDetailView, PollCreate
+from .views import PollListView, PollDetailView, PollCreate, PollUpdate
 
 urlpatterns = [
     path('', PollListView.as_view(), name='poll_list'),
     path('<int:pk>/', PollDetailView.as_view(), name='poll_detail'),
     path('create/', PollCreate.as_view(), name='poll_create'),
+    path('<int:pk>/update/', PollUpdate.as_view(), name='poll_update'),
 ]
--- ask_project/poll/views.py
+++ ask_project/poll/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import redirect
-from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import redirect, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Poll
 from .forms import PollForm
 
@@ -27,3 +27,13 @@ class PollCreate(CreateView):
         poll = form.save(commit=False)
         poll.save()
         return redirect('poll_list')
+
+
+class PollUpdate(UpdateView):
+    model = Poll
+    template_name = 'poll_form.html'
+    form_class = PollForm
+    context_key = 'poll'
+
+    def get_success_url(self):
+        return reverse('poll_detail', kwargs={'pk': self.object.pk})
